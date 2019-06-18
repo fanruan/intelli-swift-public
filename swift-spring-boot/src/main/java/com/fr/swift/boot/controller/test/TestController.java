4c6c48fd0e2763b8e199344aff8f1255a245280f
Yee <yee@fanruan.com>
2019-06-18 09:35:53 +0800

    KERNEL-706 漏斗计算去除全局字典 DynamicClassLoader新增泛型支持

--- swift-spring-boot/src/main/java/com/fr/swift/boot/controller/test/TestController.java
+++ swift-spring-boot/src/main/java/com/fr/swift/boot/controller/test/TestController.java
@@ -8,12 +8,18 @@ import com.fr.swift.base.meta.SwiftMetaDataBean;
 import com.fr.swift.basics.base.selector.ProxySelector;
 import com.fr.swift.compare.Comparators;
 import com.fr.swift.config.service.SwiftMetaDataService;
-import com.fr.swift.config.service.SwiftSegmentService;
 import com.fr.swift.db.SwiftDatabase;
 import com.fr.swift.db.Table;
+import com.fr.swift.query.QueryRunnerProvider;
 import com.fr.swift.query.aggregator.AggregatorType;
 import com.fr.swift.query.info.bean.element.DimensionBean;
 import com.fr.swift.query.info.bean.element.MetricBean;
+import com.fr.swift.query.info.bean.element.aggregation.FunnelFunctionBean;
+import com.fr.swift.query.info.bean.element.aggregation.funnel.DayFilterBean;
+import com.fr.swift.query.info.bean.element.aggregation.funnel.ParameterColumnsBean;
+import com.fr.swift.query.info.bean.element.filter.impl.NumberInRangeFilterBean;
+import com.fr.swift.query.info.bean.post.FunnelMedianInfoBean;
+import com.fr.swift.query.info.bean.post.PostQueryInfoBean;
 import com.fr.swift.query.info.bean.query.DetailQueryInfoBean;
 import com.fr.swift.query.info.bean.query.FunnelQueryBean;
 import com.fr.swift.query.info.bean.query.GroupQueryInfoBean;
@@ -22,9 +28,6 @@ import com.fr.swift.query.info.bean.type.DimensionType;
 import com.fr.swift.query.query.QueryBean;
 import com.fr.swift.query.result.SwiftResultSetUtils;
 import com.fr.swift.result.SwiftResultSet;
-import com.fr.swift.segment.Segment;
-import com.fr.swift.segment.SegmentKey;
-import com.fr.swift.segment.SegmentUtils;
 import com.fr.swift.segment.insert.HistoryBlockImporter;
 import com.fr.swift.service.AnalyseService;
 import com.fr.swift.source.ListBasedRow;
@@ -117,25 +120,33 @@ public class TestController {
         return true;
     }
 
-    @RequestMapping("swift/createTable")
-    @ResponseBody
-    public Boolean createTable() {
-        SwiftMetaData metaData = new SwiftMetaDataBean();
-        ((SwiftMetaDataBean) metaData).setId("test_yiguan");
-        ((SwiftMetaDataBean) metaData).setSwiftDatabase(SwiftDatabase.CUBE);
-        ((SwiftMetaDataBean) metaData).setTableName("test_yiguan");
-        List<SwiftMetaDataColumn> metaDataColumns = new ArrayList<SwiftMetaDataColumn>();
-        metaDataColumns.add(new MetaDataColumnBean("id", Types.VARCHAR));
-        metaDataColumns.add(new MetaDataColumnBean("currentTime", Types.TIMESTAMP));
-        metaDataColumns.add(new MetaDataColumnBean("eventType", Types.VARCHAR));
-        metaDataColumns.add(new MetaDataColumnBean("eventName", Types.VARCHAR));
-        metaDataColumns.add(new MetaDataColumnBean("json", Types.VARCHAR));
-        metaDataColumns.add(new MetaDataColumnBean("longValue", Types.BIGINT));
-
-        ((SwiftMetaDataBean) metaData).setFields(metaDataColumns);
-        SwiftContext.get().getBean(SwiftMetaDataService.class).addMetaData("test_yiguan", metaData);
-        return true;
-    }
+    private static String funnelQuery = "{\n" +
+            "  \"fetchSize\": 200,\n" +
+            "  \"aggregation\": {\n" +
+            "    \"timeWindow\": 2592000,\n" +
+            "    \"columns\": {\n" +
+//            "      \"date\": \"date\",\n" +
+            "      \"event\": \"eventType\",\n" +
+            "      \"userId\": \"id\",\n" +
+            "      \"combine\": \"combine\"\n" +
+            "    },\n" +
+            "    \"funnelEvents\": [\n" +
+            "      \"login\",\n" +
+            "      \"browseGoods\"\n" +
+//            "      \"addCart\"\n" +
+            "    ],\n" +
+            "    \"dayFilter\": {\n" +
+            "      \"column\": \"date\",\n" +
+            "      \"dayStart\": \"20180601\",\n" +
+            "      \"numberOfDays\": 30\n" +
+            "    }\n" +
+            "  },\n" +
+            "  \"postAggregations\": [],\n" +
+            "  \"sorts\": [],\n" +
+            "  \"tableName\": \"test_yiguan\",\n" +
+            "  \"dimensions\": [],\n" +
+            "  \"queryType\": \"FUNNEL\"\n" +
+            "}";
 
     @RequestMapping("swift/query")
     @ResponseBody
@@ -145,77 +156,29 @@ public class TestController {
         return queryBean;
     }
 
-    @RequestMapping("swift/importYiguan")
-    @ResponseBody
-    public Boolean testYiguanImport(String path) throws Exception {
-        final String dayStart = "20180601";
-        final SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
-        final long millisOn201861 = format.parse(dayStart).getTime();
-        Table table = com.fr.swift.db.impl.SwiftDatabase.getInstance().getTable(new SourceKey("test_yiguan"));
-        SwiftImportResultSet resultSet = new SingleStreamImportResultSet(table.getMetadata(),
-                path, new TabLineParser(false, new FileLineParser.LineParserAdaptor() {
-            @Override
-            public Row adapt(Row row) {
-                // [10003409308807394455, 1528410985, browseGoods, 浏览商品, {"name":"watch","city":"长沙","brand":"Apple","price":2608.283}, 20180608]
-                List data = new ArrayList();
-                data.add(row.getValue(0));  // id
-                data.add(row.getValue(1));  // timestamp
-                data.add(row.getValue(2));  // eventType
-                try {
-                    Map map = JsonBuilder.readValue((String) row.getValue(4), Map.class);
-                    data.add(map.get("name"));
-                    data.add(map.get("city"));
-                    data.add(map.get("brand"));
-                    Double price = (Double) map.get("price");
-                    data.add(price == null ? null : price.toString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                String day = row.getValue(row.getSize() - 1);
-                data.add(day);  // date
-                long timestamp = Long.parseLong((String) data.get(1));
-                long eventType = GLOBAL_TYPE_DICT.get(data.get(2));
-                try {
-                    long dayIndex = TimeUnit.MILLISECONDS.toDays(format.parse(day).getTime() - millisOn201861);
-                    Long combine = (timestamp << 32) | (eventType << 16) | dayIndex;
-                    data.add(combine.toString()); // combine
-                } catch (Exception ig) {
-                }
-                return new ListBasedRow(data);
-            }
-        }));
-        HistoryHashSourceAlloter alloter = new HistoryHashSourceAlloter(
-                new SourceKey("test_yiguan"),
-                new HashAllotRule(0, 10));
-        HistoryBlockImporter importer = new HistoryBlockImporter(table, alloter);
-        importer.importData(new ProgressResultSet(new LimitedResultSet(resultSet, 2000000), "test_yiguan"));
-        SwiftSegmentService service = SwiftContext.get().getBean("segmentServiceProvider", SwiftSegmentService.class);
-        List<SegmentKey> segmentKeys = service.getSegmentByKey("test_yiguan");
-        List<Segment> segments = new ArrayList<Segment>();
-        for (SegmentKey key : segmentKeys) {
-            segments.add(SegmentUtils.newSegment(key));
-        }
-        SegmentUtils.indexSegmentIfNeed(segments);
-        return true;
+    public static void main(String[] args) throws Exception {
+
+        MetricBean money = new MetricBean("money", AggregatorType.COUNT);
+        money.setFilter(new NumberInRangeFilterBean());
+        GroupQueryInfoBean bean = GroupQueryInfoBean.builder("table_a").setDimensions(new DimensionBean(DimensionType.GROUP, "name"))
+                .setAggregations(new MetricBean("money", AggregatorType.AVERAGE), new MetricBean("money", AggregatorType.MAX), money)
+                .build();
     }
 
-    @RequestMapping("swift/createYiguanTable")
+    @RequestMapping("swift/createTable")
     @ResponseBody
-    public Boolean createYiguanTable() {
+    public Boolean createTable() {
         SwiftMetaData metaData = new SwiftMetaDataBean();
         ((SwiftMetaDataBean) metaData).setId("test_yiguan");
-        ((SwiftMetaDataBean) metaData).setSwiftDatabase(SwiftDatabase.CUBE);
+        ((SwiftMetaDataBean) metaData).setSwiftSchema(SwiftDatabase.CUBE);
         ((SwiftMetaDataBean) metaData).setTableName("test_yiguan");
         List<SwiftMetaDataColumn> metaDataColumns = new ArrayList<SwiftMetaDataColumn>();
         metaDataColumns.add(new MetaDataColumnBean("id", Types.VARCHAR));
         metaDataColumns.add(new MetaDataColumnBean("currentTime", Types.TIMESTAMP));
         metaDataColumns.add(new MetaDataColumnBean("eventType", Types.VARCHAR));
-        metaDataColumns.add(new MetaDataColumnBean("name", Types.VARCHAR));
-        metaDataColumns.add(new MetaDataColumnBean("city", Types.VARCHAR));
-        metaDataColumns.add(new MetaDataColumnBean("brand", Types.VARCHAR));
-        metaDataColumns.add(new MetaDataColumnBean("price", Types.DOUBLE));
-        metaDataColumns.add(new MetaDataColumnBean("date", Types.VARCHAR));
-        metaDataColumns.add(new MetaDataColumnBean("combine", Types.BIGINT));
+        metaDataColumns.add(new MetaDataColumnBean("eventName", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("json", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("longValue", Types.BIGINT));
 
         ((SwiftMetaDataBean) metaData).setFields(metaDataColumns);
         SwiftContext.get().getBean(SwiftMetaDataService.class).addMetaData("test_yiguan", metaData);
@@ -246,19 +209,58 @@ public class TestController {
         return rows;
     }
 
-    @RequestMapping("swift/query/funnel")
+    @RequestMapping("swift/importYiguan")
     @ResponseBody
-    public Object testFunnel() throws Exception {
-        QueryBean bean = JsonBuilder.readValue(funnelQuery, FunnelQueryBean.class);
-        bean.setQueryId(UUID.randomUUID().toString());
-        AnalyseService service = ProxySelector.getInstance().getFactory().getProxy(AnalyseService.class);
-        SwiftResultSet resultSet = SwiftResultSetUtils.toSwiftResultSet(
-                service.getQueryResult(QueryBeanFactory.queryBean2String(bean)), bean);
-        List<Row> rows = new ArrayList<Row>();
-        while (resultSet.hasNext()) {
-            rows.add(resultSet.getNextRow());
-        }
-        return rows;
+    public Boolean testYiguanImport(String path) throws Exception {
+        final String dayStart = "20180601";
+        final SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        final long millisOn201861 = format.parse(dayStart).getTime();
+        Table table = com.fr.swift.db.impl.SwiftDatabase.getInstance().getTable(new SourceKey("test_yiguan"));
+        SwiftImportResultSet resultSet = new SingleStreamImportResultSet(table.getMetadata(),
+                path, new TabLineParser(false, new FileLineParser.LineParserAdaptor() {
+            @Override
+            public Row adapt(Row row) {
+                // [10003409308807394455, 1528410985, browseGoods, 浏览商品, {"name":"watch","city":"长沙","brand":"Apple","price":2608.283}, 20180608]
+                List data = new ArrayList();
+                data.add(row.getValue(0));  // id
+                data.add(Long.parseLong(row.getValue(1).toString()));  // timestamp
+                data.add(row.getValue(2));  // eventType
+                try {
+                    Map map = JsonBuilder.readValue((String) row.getValue(4), Map.class);
+                    data.add(map.get("name"));
+                    data.add(map.get("city"));
+                    data.add(map.get("brand"));
+                    Double price = (Double) map.get("price");
+                    data.add(price);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                String day = row.getValue(row.getSize() - 1);
+                data.add(day);  // date
+                long timestamp = (Long) data.get(1);
+                long eventType = GLOBAL_TYPE_DICT.get(data.get(2));
+                try {
+                    long dayIndex = TimeUnit.MILLISECONDS.toDays(format.parse(day).getTime() - millisOn201861);
+                    Long combine = (timestamp << 32) | (eventType << 16) | dayIndex;
+                    data.add(combine); // combine
+                } catch (Exception ig) {
+                }
+                return new ListBasedRow(data);
+            }
+        }));
+        HistoryHashSourceAlloter alloter = new HistoryHashSourceAlloter(
+                new SourceKey("test_yiguan"),
+                new HashAllotRule(0, 10));
+        HistoryBlockImporter importer = new HistoryBlockImporter(table, alloter);
+        importer.importData(new ProgressResultSet(new LimitedResultSet(resultSet, 2000000), "test_yiguan"));
+//        SwiftSegmentService service = SwiftContext.get().getBean("segmentServiceProvider", SwiftSegmentService.class);
+//        List<SegmentKey> segmentKeys = service.getSegmentByKey("test_yiguan");
+//        List<Segment> segments = new ArrayList<Segment>();
+//        for (SegmentKey key : segmentKeys) {
+//            segments.add(SegmentUtils.newSegment(key));
+//        }
+//        SegmentUtils.indexSegmentIfNeed(segments);
+        return true;
     }
 
     @RequestMapping("swift/query/detail")
@@ -281,33 +283,28 @@ public class TestController {
         return rows;
     }
 
-    private static String funnelQuery = "{\n" +
-            "  \"fetchSize\": 200,\n" +
-            "  \"aggregation\": {\n" +
-            "    \"timeWindow\": 2592000,\n" +
-            "    \"columns\": {\n" +
-            "      \"date\": \"date\",\n" +
-            "      \"event\": \"eventType\",\n" +
-            "      \"userId\": \"id\",\n" +
-            "      \"combine\": \"combine\"\n" +
-            "    },\n" +
-            "    \"funnelEvents\": [\n" +
-            "      \"login\",\n" +
-            "      \"browseGoods\",\n" +
-            "      \"addCart\"\n" +
-            "    ],\n" +
-            "    \"dayFilter\": {\n" +
-            "      \"column\": \"date\",\n" +
-            "      \"dayStart\": \"20180601\",\n" +
-            "      \"numberOfDays\": 30\n" +
-            "    }\n" +
-            "  },\n" +
-            "  \"postAggregations\": [],\n" +
-            "  \"sorts\": [],\n" +
-            "  \"tableName\": \"test_yiguan\",\n" +
-            "  \"dimensions\": [],\n" +
-            "  \"queryType\": \"FUNNEL\"\n" +
-            "}";
+    @RequestMapping("swift/createYiguanTable")
+    @ResponseBody
+    public Boolean createYiguanTable() {
+        SwiftMetaData metaData = new SwiftMetaDataBean();
+        ((SwiftMetaDataBean) metaData).setId("test_yiguan");
+        ((SwiftMetaDataBean) metaData).setSwiftSchema(SwiftDatabase.CUBE);
+        ((SwiftMetaDataBean) metaData).setTableName("test_yiguan");
+        List<SwiftMetaDataColumn> metaDataColumns = new ArrayList<SwiftMetaDataColumn>();
+        metaDataColumns.add(new MetaDataColumnBean("id", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("currentTime", Types.TIMESTAMP));
+        metaDataColumns.add(new MetaDataColumnBean("eventType", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("name", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("city", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("brand", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("price", Types.DOUBLE));
+        metaDataColumns.add(new MetaDataColumnBean("date", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("combine", Types.BIGINT));
+
+        ((SwiftMetaDataBean) metaData).setFields(metaDataColumns);
+        SwiftContext.get().getBean(SwiftMetaDataService.class).addMetaData("test_yiguan", metaData);
+        return true;
+    }
 
     private static final List<String> events = Arrays.asList("addCart", "browseGoods", "collectionGoods", "confirm", "consultGoods", "evaluationGoods", "login", "order", "orderPayment", "reminding", "searchGoods", "shareGoods", "startUp", "unsubscribeGoods", "viewOrder");
 
@@ -317,5 +314,23 @@ public class TestController {
         for (String key : events) {
             GLOBAL_TYPE_DICT.put(key, (long) (GLOBAL_TYPE_DICT.size() + 1));
         }
+    }
+
+    @RequestMapping("swift/query/funnel")
+    @ResponseBody
+    public Object testFunnel() throws Exception {
+        DayFilterBean dayFilter = new DayFilterBean("date", "20180601", 30);
+        ParameterColumnsBean paramColumn = new ParameterColumnsBean("id", "eventType", "combine");
+        FunnelFunctionBean aggregation = new FunnelFunctionBean(2592000, dayFilter, paramColumn, Arrays.asList("login", "browseGoods"));
+        FunnelQueryBean bean = new FunnelQueryBean(aggregation);
+        bean.setPostAggregations(Arrays.<PostQueryInfoBean>asList(new FunnelMedianInfoBean()));
+        bean.setQueryId(UUID.randomUUID().toString());
+        bean.setTableName("test_yiguan");
+        SwiftResultSet resultSet = QueryRunnerProvider.getInstance().query(bean);
+        List<Row> rows = new ArrayList<Row>();
+        while (resultSet.hasNext()) {
+            rows.add(resultSet.getNextRow());
+        }
+        return rows;
     }
 }
