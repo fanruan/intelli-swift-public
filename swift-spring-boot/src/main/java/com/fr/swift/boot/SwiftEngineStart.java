6bec7dfef9b1d9f2f6fed6761be9dc5a7e36b7fc
lucifer <lucifer@fanruan.com>
2020-03-16 18:22:01 +0800

    KERNEL-3452 1、无用代码移除、复杂代码整理重写 2、数据库相关重写

--- swift-spring-boot/src/main/java/com/fr/swift/boot/SwiftEngineStart.java
+++ swift-spring-boot/src/main/java/com/fr/swift/boot/SwiftEngineStart.java
@@ -2,21 +2,10 @@ package com.fr.swift.boot;
 
 import com.fineio.FineIO;
 import com.fr.swift.SwiftContext;
+import com.fr.swift.boot.log.SwiftLog4jLoggers;
 import com.fr.swift.boot.register.BootRegister;
-import com.fr.swift.bytebuddy.DynamicClassLoader;
-import com.fr.swift.cluster.listener.NodeStartedListener;
-import com.fr.swift.config.PublicConfig;
-import com.fr.swift.cube.queue.ProviderTaskManager;
-import com.fr.swift.event.ClusterEvent;
-import com.fr.swift.event.ClusterEventType;
-import com.fr.swift.event.ClusterListenerHandler;
-import com.fr.swift.event.ClusterType;
 import com.fr.swift.log.FineIoLogger;
-import com.fr.swift.log.SwiftLog4jLoggers;
 import com.fr.swift.log.SwiftLoggers;
-import com.fr.swift.property.SwiftProperty;
-import com.fr.swift.service.local.LocalManager;
-import com.fr.swift.service.local.ServerManager;
 
 /**
  * This class created on 2018/6/12
@@ -29,27 +18,18 @@ public class SwiftEngineStart {
 
     public static void start(String[] args) {
         try {
-            ClassLoader loader = new DynamicClassLoader(SwiftEngineStart.class.getClassLoader());
-            BootRegister.registerEntity(loader);
+            BootRegister.registerEntity();
             BootRegister.registerExecutorTask();
 
             SwiftLoggers.setLoggerFactory(new SwiftLog4jLoggers());
-            ClusterListenerHandler.addInitialListener(new SwiftClusterListener());
             SwiftContext.get().init();
 
-            ClusterListenerHandler.addInitialListener(NodeStartedListener.INSTANCE);
             FineIO.setLogger(new FineIoLogger());
-            ProviderTaskManager.start();
             SwiftCommandParser.parseCommand(args);
             BootRegister.registerProxy();
             BootRegister.registerExecutorTask();
-            PublicConfig.load();
+//            PublicConfig.load();
 
-            SwiftContext.get().getBean(LocalManager.class).startUp();
-            if (SwiftProperty.getProperty().isCluster()) {
-                ClusterListenerHandler.handlerEvent(new ClusterEvent(ClusterEventType.JOIN_CLUSTER, ClusterType.CONFIGURE));
-            }
-            SwiftContext.get().getBean(ServerManager.class).startUp();
             BootRegister.registerListener();
 
             SwiftLoggers.getLogger().info("Swift engine start successful");
