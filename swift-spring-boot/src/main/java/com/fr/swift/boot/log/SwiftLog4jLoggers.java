4c43cb73c1d72628ae38aa5f8929fc703b64ca53
lucifer <lucifer@fanruan.com>
2020-06-09 16:44:19 +0800

    无jira任务，log4j文件可配置

--- swift-spring-boot/src/main/java/com/fr/swift/boot/log/SwiftLog4jLoggers.java
+++ swift-spring-boot/src/main/java/com/fr/swift/boot/log/SwiftLog4jLoggers.java
@@ -2,13 +2,7 @@ package com.fr.swift.boot.log;
 
 import com.fr.swift.log.SwiftLogger;
 import com.fr.swift.log.SwiftLoggerFactory;
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.DailyRollingFileAppender;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
-
-import java.io.IOException;
 
 /**
  * @author anchore
@@ -20,14 +14,14 @@ public class SwiftLog4jLoggers implements SwiftLoggerFactory<Void> {
 
     private static SwiftLogger newLogger() {
         Logger logger = Logger.getLogger(SwiftLog4jLogger.class);
-        logger.setLevel(Level.INFO);
-        logger.setAdditivity(false);
-        PatternLayout layout = new PatternLayout("%d{yy-M-d H:m:s.S} %t %p [%C{1}.%M] %m%n");
-        logger.addAppender(new ConsoleAppender(layout));
-        try {
-            logger.addAppender(new DailyRollingFileAppender(layout, "logs/swift", "yy-MM-dd'.log'"));
-        } catch (IOException ignore) {
-        }
+//        logger.setLevel(Level.INFO);
+//        logger.setAdditivity(false);
+//        PatternLayout layout = new PatternLayout("%d{yy-M-d H:m:s.S} %t %p [%C{1}.%M] %m%n");
+//        logger.addAppender(new ConsoleAppender(layout));
+//        try {
+//            logger.addAppender(new DailyRollingFileAppender(layout, "logs/swift", "yy-MM-dd'.log'"));
+//        } catch (IOException ignore) {
+//        }
         return new SwiftLog4jLogger(logger);
     }
 
