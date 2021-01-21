d5daf9ce2f01f6c77a0218d0a8387b318867722f
xiqiu <xiqiu@fanruan.com>
2021-01-21 15:35:30 +0800

    REPORT-47032    修改同步prc的传输方式

--- swift-rpc-netty/src/main/java/com/fr/swift/netty/rpc/client/sync/SyncRpcClientHandler.java
+++ swift-rpc-netty/src/main/java/com/fr/swift/netty/rpc/client/sync/SyncRpcClientHandler.java
@@ -5,12 +5,14 @@ import com.fr.swift.basic.Response;
 import com.fr.swift.log.SwiftLogger;
 import com.fr.swift.log.SwiftLoggers;
 import com.fr.swift.netty.rpc.client.AbstractRpcClientHandler;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * This class created on 2018/6/12
@@ -21,11 +23,9 @@ import java.util.concurrent.CountDownLatch;
  */
 @ChannelHandler.Sharable
 public class SyncRpcClientHandler extends AbstractRpcClientHandler<Response> {
-
+    private static final Map<String, SyncRpcMessageEntity> SYNC_RPC_MESSAGE_ENTITY_MAP = new ConcurrentHashMap<>();
     public static final String POOL_KEY = "SyncRpcClientHandler";
     private static final SwiftLogger LOGGER = SwiftLoggers.getLogger(SyncRpcClientHandler.class);
-    private Response response;
-    private CountDownLatch countDownLatch;
 
     public SyncRpcClientHandler(String address) {
         super(address);
@@ -33,15 +33,25 @@ public class SyncRpcClientHandler extends AbstractRpcClientHandler<Response> {
 
     @Override
     public void channelRead0(ChannelHandlerContext ctx, Response response) {
-        this.response = response;
-        countDownLatch.countDown();
-        LOGGER.debug("Receive response : " + response.getRequestId());
+        if (SYNC_RPC_MESSAGE_ENTITY_MAP.containsKey(response.getRequestId())) {
+            SyncRpcMessageEntity syncRpcMessageEntity = SYNC_RPC_MESSAGE_ENTITY_MAP.get(response.getRequestId());
+            syncRpcMessageEntity.setResponse(response);
+            syncRpcMessageEntity.getCountDownLatch().countDown();
+            LOGGER.info("message arrive");
+        } else {
+            LOGGER.info("message is to late,such a waste");
+        }
+        LOGGER.info("Receive response :response id is {} ", response.getRequestId());
     }
 
+    @Override
     public Response send(final Request request) throws Exception {
-        countDownLatch = new CountDownLatch(1);
-        channel.writeAndFlush(request).sync().addListener((ChannelFutureListener) channelFuture -> LOGGER.debug("Send request : " + request.getRequestId()));
-        countDownLatch.await();
+        SyncRpcMessageEntity syncRpcMessageEntity = new SyncRpcMessageEntity(request, new CountDownLatch(1));
+        channel.writeAndFlush(request).sync().addListener((ChannelFutureListener) channelFuture -> LOGGER.info("Send request : " + request.getRequestId()));
+        SYNC_RPC_MESSAGE_ENTITY_MAP.put(request.getRequestId(), syncRpcMessageEntity);
+        syncRpcMessageEntity.getCountDownLatch().await(5, TimeUnit.MINUTES);
+        Response response = SYNC_RPC_MESSAGE_ENTITY_MAP.get(request.getRequestId()).getResponse();
+        SYNC_RPC_MESSAGE_ENTITY_MAP.remove(request.getRequestId());
         return response;
     }
 }
