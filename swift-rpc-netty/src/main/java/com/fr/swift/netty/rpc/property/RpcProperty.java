208448bd4f760b32e2ac855b548e34ca61a2bff5
HengJiang <Heng.Jiang@fanruan.com>
2021-01-29 16:22:45 +0800

    REPORT-47521 【swift】jdbc查询正式服数据，查询为空 * 补充rpc对象池日志信息以及外部配置,正式服观察使用情况; * python端请求的tcp加上IP白名单;

---
+++ swift-rpc-netty/src/main/java/com/fr/swift/netty/rpc/property/RpcProperty.java
@@ -0,0 +1,94 @@
+package com.fr.swift.netty.rpc.property;
+
+import com.fr.swift.config.ConfigInputUtil;
+import com.fr.swift.util.Crasher;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Heng.J
+ * @date 2021/1/26
+ * @description
+ * @since swift-1.2.0
+ */
+public class RpcProperty {
+
+    private Properties properties;
+
+    /**
+     * 数量控制参数
+     */
+    private int minIdlePerKey;
+
+    private int maxIdlePerKey;
+
+    private int maxTotalPerKey;
+
+    private int maxTotal;
+
+    private long minEvictableIdleTimeMillis;  // 连接最小空闲时间,超时空闲被移除
+
+    /**
+     * 检测参数
+     */
+    private long timeBetweenEvictionRunsMillis; // 空闲链接检测线程检测的周期
+
+    private RpcProperty() {
+        initProperties();
+    }
+
+    private static final RpcProperty INSTANCE = new RpcProperty();
+
+    public static RpcProperty get() {
+        return INSTANCE;
+    }
+
+    private void initProperties() {
+        properties = new Properties();
+        try (InputStream inputStream = ConfigInputUtil.getConfigInputStream("rpc.properties")) {
+            properties.load(inputStream);
+            minIdlePerKey = Integer.parseInt((String) properties.getOrDefault("minIdlePerKey", "0"));
+            maxIdlePerKey = Integer.parseInt((String) properties.getOrDefault("maxIdlePerKey", "8"));
+            maxTotalPerKey = Integer.parseInt((String) properties.getOrDefault("maxTotalPerKey", "8"));
+            maxTotal = Integer.parseInt((String) properties.getOrDefault("maxTotal", "-1"));
+            minEvictableIdleTimeMillis = Long.parseLong((String) properties.getOrDefault("minEvictableIdleTimeMillis", String.valueOf(TimeUnit.MINUTES.toMillis(30))));
+            timeBetweenEvictionRunsMillis = Long.parseLong((String) properties.getOrDefault("timeBetweenEvictionRunsMillis", String.valueOf(TimeUnit.MINUTES.toMillis(30))));
+        } catch (IOException e) {
+            // GenericKeyedObjectPoolConfig default
+            minIdlePerKey = 0;
+            maxIdlePerKey = 8;
+            maxTotalPerKey = 8;
+            maxTotal = -1;
+            minEvictableIdleTimeMillis = TimeUnit.MINUTES.toMillis(30);  //1000L * 60L * 30L
+            timeBetweenEvictionRunsMillis = TimeUnit.MINUTES.toMillis(30);
+            Crasher.crash(e);
+        }
+    }
+
+    public int getMinIdlePerKey() {
+        return minIdlePerKey;
+    }
+
+    public int getMaxIdlePerKey() {
+        return maxIdlePerKey;
+    }
+
+    public int getMaxTotalPerKey() {
+        return maxTotalPerKey;
+    }
+
+    public int getMaxTotal() {
+        return maxTotal;
+    }
+
+    public long getMinEvictableIdleTimeMillis() {
+        return minEvictableIdleTimeMillis;
+    }
+
+    public long getTimeBetweenEvictionRunsMillis() {
+        return timeBetweenEvictionRunsMillis;
+    }
+}
